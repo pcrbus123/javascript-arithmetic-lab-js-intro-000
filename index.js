930960c728733569cdbe1a6520e05f170516002c
Pnina Breslav <pcrbus123@gmail.com>
2018-10-06 18:16:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ function add(a,b){return a+=b}
 function subtract(a,b){return a-=b}
 function multiply(a,b){return a*=b}
 function divide(a,b){return a/=b}
-function inc(n){
-  n++;return n
-}
-
-function dec(n){return --number}
+function inc(n){n++;return n}
+function dec(n){n--;return n}
