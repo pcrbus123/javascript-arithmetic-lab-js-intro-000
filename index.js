de02c8d205ce4baa8d81bcb6356cd7d354749469
Pnina Breslav <pcrbus123@gmail.com>
2018-10-06 18:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a,b){return a+=b}
 function subtract(a,b){return a-=b}
 function multiply(a,b){return a*=b}
 function divide(a,b){return a/=b}
-function increment(){
+function increment(n){
   var n = 0;
   n++;
   return n;
