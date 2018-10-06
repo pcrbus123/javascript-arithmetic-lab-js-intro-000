d3307d4b88a3fe2fcef4739f89442bfe5bf5b3a7
Pnina Breslav <pcrbus123@gmail.com>
2018-10-06 18:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function divide(a,b){return a/=b}
 function inc(n){n++;return n}
 function dec(n){n--;return n}
 function makeInt(n){parseInt('n', 10); return n}
+function preser(n){parseFloat(n);return n}
