2aa45c1c15bc05078e2ea71367091e2b382a206a
Pnina Breslav <pcrbus123@gmail.com>
2018-10-06 18:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function multiply(a,b){return a*=b}
 function divide(a,b){return a/=b}
 function inc(n){n++;return n}
 function dec(n){n--;return n}
-function makeInt(n){parseInt('n', '10'); return n}
+function makeInt(n){parseInt('n', 10); return n}
